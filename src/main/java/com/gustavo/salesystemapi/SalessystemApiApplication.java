e36a65f1f76e1d79929ad19d3c4a582ae4f06710
guseusmor <gemtecsi@gmail.com>
2021-03-01 20:31:24 -0300

    First commit

---
+++ src/main/java/com/gustavo/salesystemapi/SalessystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.salesystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalessystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalessystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/salesystemapi/SalessystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.salesystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalessystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
