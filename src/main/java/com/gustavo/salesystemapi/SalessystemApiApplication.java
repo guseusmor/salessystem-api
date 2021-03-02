f6ed9ee493c307f18433b2d0c947d655625a79d3
guseusmor <gemtecsi@gmail.com>
2021-03-01 22:01:03 -0300

    Created instantiation operation

--- src/main/java/com/gustavo/salesystemapi/SalessystemApiApplication.java
+++ src/main/java/com/gustavo/salesystemapi/SalessystemApiApplication.java
@@ -1,13 +1,33 @@
 package com.gustavo.salesystemapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SalessystemApiApplication {
+import com.gustavo.salesystemapi.domain.Categoria;
+import com.gustavo.salesystemapi.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SalessystemApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SalessystemApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
